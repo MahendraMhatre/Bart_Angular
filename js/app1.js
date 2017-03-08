2c29677818e227d19330ed8ebf564bc1836bb045
MahendraMhatre <mmhatre@scu.edu>
2017-03-08 12:14:02 -0800

    Final Code without Errors

--- js/app1.js
+++ js/app1.js
@@ -42,36 +42,156 @@
             $http.get("http://bart.mahendramhatre.com/station?source="+station).success(function(result) {
                 console.log("Success", result);
                 $scope.resultS = result;
+
             }).error(function() {
                 console.log("error");
             });
         };
 
-        
+
+        $scope.lat1 =  37.765062;
+        $scope.long1 = -122.419694;
+
+        var cities = [
+            {
+                city : 'SFO',
+                desc : 'This city is live!',
+                lat :  this.lat1,
+                long : this.long1
+            }
+        ];
+        var mapOptions = {
+            zoom: 15,
+            center: new google.maps.LatLng( $scope.lat1, $scope.long1),
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+        }
+
+
+        var marker = "";
+        $scope.map ="";
+        $scope.createMarker = function (){
+            mapOptions.center = new google.maps.LatLng($scope.lat1, $scope.long1);
+            $scope.map = new google.maps.Map(document.getElementById('mapElement'), mapOptions);
+            marker = new google.maps.Marker({
+                map: $scope.map,
+                position: new google.maps.LatLng($scope.lat1, $scope.long1)
+
+            });
+
+        };
+
+        $scope.change = function(sLat, sLong) {
+            $scope.lat1 = sLat;
+            $scope.long1 = sLong;
+        };
+
+        $scope.createMarker();
+
 
     });
 
-    app.controller('detailsCtrl', function($scope, $http, $interval) {
+    app.filter('formatTimer', function () {
+        return function (input) {
+            function z(n) {
+                return (n < 10 ? '0' : '') + n;
+            }
 
-        this.source = "ASHB";
-        this.dest = "CIVC";
+            var seconds = input % 60;
+            var minutes = Math.floor(input / 60);
+            var hours = Math.floor(minutes / 60);
+            return (z(hours) + ':' + z(minutes) + ':' + z(seconds));
+        };
+    });
+
+
+    app.controller('detailsCtrl', function($scope, $http, $interval, $timeout) {
+
 
         this.isTrue = function() {
             return false;
         };
 
 
-        $interval(this.getDetails = function(org , dest) {
+        $scope.timeFound = false;
+        $scope.noTrainFound = false;
+        $scope.countTime = 0;
+        var mytimeout;
+        $scope.getTimer = function (ans) {
+            var currentTime = new Date().getTime();
+            var loopRun = true;
+            var realDate = currentTime;
+            angular.forEach(ans.schedule.request.trip, function (trip) {
+                if(loopRun){
+                    var dateString = trip["@attributes"].origTimeDate + trip["@attributes"].origTimeMin;
+                    realDate = new Date(dateString);
+                    realDate = realDate.getTime();
+                    if(realDate > currentTime) {
+                        loopRun = false;
+                    }
+                }
+
+            });
+
+            var tempCounter = 60 + Math.floor((realDate - currentTime) / 1000);
+            if( tempCounter < 0) {
+                $scope.countTime = 0;
+                $scope.noTrainFound = true;
+                $scope.timeFound = false;
+            } else {
+                $scope.noTrainFound = false;
+                $scope.countTime = tempCounter;
+                $scope.timeFound = true;
+
+            }
+            if (!(mytimeout === undefined)) {
+                $timeout.cancel(mytimeout);
+
+            }
+            $scope.onTimeout = function () {
+                if($scope.countTime <= 0) {
+                    $scope.countTime = 0;
+                    $scope.getRoutes();
+                    $scope.noTrainFound = true;
+                    $scope.timeFound = false;
+
+                }
+                else {
+                    $scope.countTime--;
+                    $scope.noTrainFound = false;
+                    $scope.timeFound = true;
+
+                }
+                mytimeout = $timeout($scope.onTimeout, 1000);
+            };
+            mytimeout = $timeout($scope.onTimeout, 1000);
+        };
+
+
+        $interval($scope.getDetails = function(org , dest) {
 
             if(!(org === undefined) && !(dest === undefined)) {
+
                 $http.get("http://bart.mahendramhatre.com/trains?source=" + org.abbr + "&dest=" + dest.abbr).success(function (result) {
                     console.log("Success", result);
                     $scope.resultTrip = result;
+
+                    if(!($scope.resultTrip.schedule === undefined) ){
+                        $scope.getTimer($scope.resultTrip);
+                    }
+                    else {
+                        $scope.countTime= 0;
+                        $timeout.cancel(mytimeout);
+                    }
+
                 }).error(function () {
                     console.log("error");
                 });
+
+
             };
+
         },30000);
+        
 
     });
 
@@ -88,8 +208,8 @@
 
 
     app.controller('TabController', function(){
-        this.tab = 1;
-        
+        this.tab = 2;
+
         this.setTab = function(newValue){
             this.tab = newValue;
         };
@@ -129,22 +249,19 @@
         }
 
 
-        $scope.markers = [];
-
-        var infoWindow = new google.maps.InfoWindow();
-
+        var marker = "";
+        $scope.map ="";
         this.createMarker = function (){
             mapOptions.center = new google.maps.LatLng(this.lat1, this.long1);
-            $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-            var marker = new google.maps.Marker({
+            $scope.map = new google.maps.Map(document.getElementById('mapElement'), mapOptions);
+            marker = new google.maps.Marker({
                 map: $scope.map,
-                position: new google.maps.LatLng(this.lat1, this.long1),
-                title: cities[0].city
+                position: new google.maps.LatLng(this.lat1, this.long1)
+
             });
-            marker.content = '<div class="infoWindowContent">' + cities[0].desc + '</div>';
 
         }
-        this.createMarker();
+
 
     });
 
@@ -191,7 +308,7 @@
 
             // get route from A to B
 
-            var directionsService = new google.maps.DirectionsService();
+            var directionsService = new google.maps.DirectionsService;
             var directionsDisplay = new google.maps.DirectionsRenderer({
                 map: map1
             });
@@ -206,7 +323,6 @@
                 origin: pointA,
                 destination: pointB,
                 avoidTolls: true,
-                avoidHighways: false,
                 travelMode: google.maps.TravelMode.TRANSIT
             };
 
